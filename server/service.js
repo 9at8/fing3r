d970a4e522ef69dc4ad8fdc2a7d9f7b346607f1e
Yash Mathur <yash97mathur@gmail.com>
2017-11-12 14:34:51 -0500

    write fingers to file

--- server/service.js
+++ server/service.js
@@ -1,8 +1,27 @@
 const express = require('express')
+var fs = require('fs');
 const app = express()
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
+})
+
+app.get('/finger/:fingerer/:fingeree', (req, res) => {
+  fs.writeFile(`/home/users/${req.params.fingerer}/config.finger`, `${req.params.fingeree}`, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
+
+  fs.writeFile(`/home/users/${req.params.fingerer}/config.finger`, `${req.params.fingeree}`, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
 })
 
 app.listen("49153", () => {
